8617104db922b88d7baac556b1cc0588b55ff566
candidate_Yacine_Chalabi <yacinec.pro@gmail.com>
2022-03-24 13:11:27 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { useTranslation } from 'react-i18next';
 
 function App() {
+  // use the t method to access to the /public/locales folder which contains the translation
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (lang) => {
+    i18n.changeLanguage(lang);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <button onClick={() => changeLanguage('en')}>EN</button>
+      <button onClick={() => changeLanguage('fr')}>FR</button>
+      <hr />
+      <h2>{t('title')}</h2>
+      <p>{t('description.part1')}</p>
+      <p>{t('description.part2')}</p>
     </div>
   );
 }
